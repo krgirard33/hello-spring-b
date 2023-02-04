a83d04b6ddf6f76f2f44044037f7b57d5204dadd
krgirard33 <ken.girard@gmail.com>
2023-02-04 00:46:01 -0600

    Moved @ResponseBody & @RequestMapping("hello") to the top of the Class, removed all @ResponseBodys

--- src/main/java/org/launchcode/hellospring/controller/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controller/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@ResponseBody
+@RequestMapping("hello")
 public class HelloController {
 
     // Handles request at path /hello
@@ -13,28 +15,25 @@ public class HelloController {
 //        return "Hello, Spring!";
 //    }
 
-    // Handles request at path /goodbye
+    // Handles request at path /hello/goodbye
     @GetMapping("goodbye")
-    @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
     }
+    // lives at /hello/hello
     // Handles request of the form /hello?=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value="hello")
-    @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
-
+    // lives at /hello/LaunchCode
     // Handles request of the form /hello/LaunchCode
-    @GetMapping("hello/{name}")
-    @ResponseBody
+    @GetMapping("{name}")
     public String helloWithPathParam(@PathVariable String name) {
         return "Hello " + name + "! How do you like this path?";
     }
-
+    // lives at /hello/form
     @GetMapping("form")
-    @ResponseBody
     public String helloForm() {
         String html =
                 "<html>" +
