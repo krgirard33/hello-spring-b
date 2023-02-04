9425e4bd59b0bcb562c6d181588cf778e53641c8
krgirard33 <ken.girard@gmail.com>
2023-02-04 00:18:42 -0600

    Modified helloForm with a post action, also updated helloWithQueryParam to use @RequestMapping rather than @GetMapping

--- src/main/java/org/launchcode/hellospring/controller/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controller/HelloController.java
@@ -1,10 +1,7 @@
 package org.launchcode.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
@@ -23,7 +20,7 @@ public class HelloController {
         return "Goodbye, Spring!";
     }
     // Handles request of the form /hello?=LaunchCode
-    @GetMapping("hello")
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value="hello")
     @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
@@ -42,7 +39,7 @@ public class HelloController {
         String html =
                 "<html>" +
                         "<body>" +
-                        "<form method = 'get' action = 'hello'>" +
+                        "<form method = 'post' action = 'hello'>" +
                         "<input type = 'text' name = 'name' />" +
                         "<input type = 'submit' value = 'Greet Me!' />" +
                         "</form>" +
